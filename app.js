5096bc074aa5dc204ee98f8ca8050b49905bcbaf
Braden <bmw02002turbo@gmail.com>
2021-01-16 12:53:16 -0800

    Next step, moved from app.js template to index.html

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const app = Vue.createApp({
   // Data, functions
-  template: "<h2>I am template</h2>",
+  //template: "<h2>I am template</h2>",
 })
 
 // Grants Vue complete access to everything in #app element (nothing outside)
