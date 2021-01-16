ebc192908282864fdd21778db13e1efbdb60485b
Braden <bmw02002turbo@gmail.com>
2021-01-16 12:50:21 -0800

    Created template

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
-const app = Vue.createApp()
+const app = Vue.createApp(){
+    template: "<h2>I am template</h2>"
+}
 
 // Grants Vue complete access to everything in #app element (nothing outside)
 app.mount("#app")
