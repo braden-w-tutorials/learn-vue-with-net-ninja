1217d6e280bc28dbef43548ab8bfbd64bd6bb5cf
Braden <bmw02002turbo@gmail.com>
2021-01-16 12:57:42 -0800

    Added reactivity

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 const app = Vue.createApp({
   // Data, functions
   //template: "<h2>I am template</h2>",
+  data() {
+    return {
+      title: "The Final Empire",
+      author: "Brandon Sanderson",
+      age: 45,
+    }
+  },
 })
 
 // Grants Vue complete access to everything in #app element (nothing outside)
