b3bd95498864f853e4958625388b46a03f81f66c
Braden <bmw02002turbo@gmail.com>
2021-01-16 13:18:51 -0800

    Added mouse events

--- app.js
+++ app.js
@@ -1,25 +1,29 @@
 const app = Vue.createApp({
-  // Data, functions
-  //template: "<h2>I am template</h2>",
-  data() {
-    return {
-      showBooks: true,
-      title: "The Final Empire",
-      author: "Brandon Sanderson",
-      age: 45,
-    }
-  },
-  methods: {
-    changeTitle(title) {
-      //  Use this
-      //   this.title = "Words of Radiance"
-      this.title = title
+    // Data, functions
+    //template: "<h2>I am template</h2>",
+    data() {
+        return {
+            showBooks: true,
+            title: "The Final Empire",
+            author: "Brandon Sanderson",
+            age: 45,
+        };
     },
-    toggleShowBooks() {
-      this.showBooks = !this.showBooks
+    methods: {
+        changeTitle(title) {
+            //  Use this
+            //   this.title = "Words of Radiance"
+            this.title = title;
+        },
+        toggleShowBooks() {
+            this.showBooks = !this.showBooks;
+        },
+
+        handleEvent() {
+            console.log("event");
+        },
     },
-  },
-})
+});
 
 // Grants Vue complete access to everything in #app element (nothing outside)
-app.mount("#app")
+app.mount("#app");
