2c4ba9a28f8b73639cca02ace92b2077c8f1eed3
Braden <bmw02002turbo@gmail.com>
2021-01-16 12:52:03 -0800

    Fixed parenthesis

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-const app = Vue.createApp(){
-    // Data, functions
-    template: '<h2>I am template</h2>'
-}
+const app = Vue.createApp({
+  // Data, functions
+  template: "<h2>I am template</h2>",
+})
 
 // Grants Vue complete access to everything in #app element (nothing outside)
 app.mount("#app")
