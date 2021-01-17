6743e1d0c8248d32a082dcb2f1f4a011a23ff9b0
Braden <bmw02002turbo@gmail.com>
2021-01-17 10:22:05 -0800

    Added props for header

--- modal-project/src/App.vue
+++ modal-project/src/App.vue
@@ -1,42 +1,42 @@
 <template>
-  <h1>{{ title }}</h1>
-  <input type="text" ref="name" />
-  <button @click="handleClick">Click me</button>
-  <Modal />
+    <h1>{{ title }}</h1>
+    <input type="text" ref="name" />
+    <button @click="handleClick">Click me</button>
+    <Modal header="Hello World" />
 </template>
 
 <script>
 import Modal from "./components/Modal.vue";
 export default {
-  name: "App",
-  components: { Modal },
-  data() {
-    return {
-      title: "My First Vue App :)"
-    };
-  },
-  methods: {
-    handleClick() {
-      console.log(this.$refs.name);
-      this.$refs.name.classList.add("active");
-      this.$refs.name.focus();
+    name: "App",
+    components: { Modal },
+    data() {
+        return {
+            title: "My First Vue App :)"
+        };
+    },
+    methods: {
+        handleClick() {
+            console.log(this.$refs.name);
+            this.$refs.name.classList.add("active");
+            this.$refs.name.focus();
+        }
     }
-  }
 };
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
 }
 h1 {
-  border-bottom: 1px solid #ddd;
-  display: inline-block;
-  padding-bottom: 10px;
+    border-bottom: 1px solid #ddd;
+    display: inline-block;
+    padding-bottom: 10px;
 }
 </style>
