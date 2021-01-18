765f9598f180fb6092968379ab338e01f3c9c2b2
Braden <bmw02002turbo@gmail.com>
2021-01-17 16:16:38 -0800

    Created Vue App with 2 child components

---
+++ reaction-timer-game/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <h1>Reaction Timing Game</h1>
+</template>
+
+<script>
+export default {
+  name: "App",
+  components: {Results, Click Me},
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #444;
+  margin-top: 60px;
+}
+</style>
