67810ec9de8a433821599ef3c0f185237644b8eb
Braden <bmw02002turbo@gmail.com>
2021-01-17 16:19:54 -0800

    Created button with method

--- reaction-timer-game/src/App.vue
+++ reaction-timer-game/src/App.vue
@@ -1,11 +1,25 @@
 <template>
   <h1>Reaction Timing Game</h1>
+  <button @click="startTimer">Play</button>
 </template>
 
 <script>
 export default {
   name: "App",
-  components: {Results, Click Me},
+  components: {},
+  data() {
+    return {
+      isPlaying: false,
+      delay: null,
+    }
+  },
+  methods: {
+    startTimer() {
+      this.isPlaying = true
+      this.delay = 2000 + Math.random() * 5000
+      console.log(this.delay)
+    },
+  },
 }
 </script>
 
