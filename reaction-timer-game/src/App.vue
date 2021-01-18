f66ed791e83367280004ec89d2bcc1ec395cfb62
Braden <bmw02002turbo@gmail.com>
2021-01-17 16:35:19 -0800

    Added changes to ClickMe and APp

--- reaction-timer-game/src/App.vue
+++ reaction-timer-game/src/App.vue
@@ -1,35 +1,37 @@
 <template>
-  <h1>Reaction Timing Game</h1>
-  <button @click="startTimer">Play</button>
+    <h1>Reaction Timing Game</h1>
+    <button @click="startTimer" :disabled="isPlaying">Play</button>
+    <ClickMe v-if="isPlaying" :delay="delay" />
 </template>
 
 <script>
+import ClickMe from "./components/ClickMe.vue";
+
 export default {
-  name: "App",
-  components: {},
-  data() {
-    return {
-      isPlaying: false,
-      delay: null,
-    }
-  },
-  methods: {
-    startTimer() {
-      this.isPlaying = true
-      this.delay = 2000 + Math.random() * 5000
-      console.log(this.delay)
+    name: "App",
+    components: { ClickMe },
+    data() {
+        return {
+            isPlaying: false,
+            delay: null
+        };
     },
-  },
-}
+    methods: {
+        startTimer() {
+            this.isPlaying = true;
+            this.delay = 2000 + Math.random() * 5000;
+        }
+    }
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #444;
-  margin-top: 60px;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #444;
+    margin-top: 60px;
 }
 </style>
