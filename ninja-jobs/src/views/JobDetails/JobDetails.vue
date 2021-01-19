e4ccf340f2ad0922325663ea4f58b5004e1feaff
Braden <bmw02002turbo@gmail.com>
2021-01-18 22:07:18 -0800

    Uses id instead of $route.params

--- ninja-jobs/src/views/JobDetails/JobDetails.vue
+++ ninja-jobs/src/views/JobDetails/JobDetails.vue
@@ -1,7 +1,7 @@
 <template>
   <h1>Job Details Landing Page</h1>
   <!-- Passes id from url as a param of $route -->
-  <p>The job id is {{ $route.params.id }}</p>
+  <p>The job id is {{ id }}</p>
 </template>
 
 <script>
