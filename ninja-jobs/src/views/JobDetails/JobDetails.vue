b71defb147c6efd0160d7fb798ccb3f221bc6af2
Braden <bmw02002turbo@gmail.com>
2021-01-18 23:31:12 -0800

    Added fetch to JobDetails

--- ninja-jobs/src/views/JobDetails/JobDetails.vue
+++ ninja-jobs/src/views/JobDetails/JobDetails.vue
@@ -1,12 +1,25 @@
 <template>
-  <h1>Job Details Landing Page</h1>
+  <!-- <h1>Job Details Landing Page</h1> -->
+  <h1>{{ job.title }}</h1>
   <!-- Passes id from url as a param of $route -->
   <p>The job id is {{ id }}</p>
+  <p>{{ job.details }}</p>
 </template>
 
 <script>
 export default {
   props: ["id"],
+  data() {
+    return {
+      job: null,
+    }
+  },
+  mounted() {
+    fetch("http://localhost:3000/jobs" + this.id)
+      .then((res) => res.json())
+      .then((data) => (this.job = data))
+      .catch((err) => console.log(err.message))
+  },
   //   data() {
   //     return {
   //       id: this.$route.params.id,
