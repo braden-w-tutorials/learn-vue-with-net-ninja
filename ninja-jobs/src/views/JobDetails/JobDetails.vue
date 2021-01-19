1102ca818ddf18cae0a6b4065f1f0d1fbfb128df
Braden <bmw02002turbo@gmail.com>
2021-01-18 21:59:30 -0800

    Added JobDetails from param of router

---
+++ ninja-jobs/src/views/JobDetails/JobDetails.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1>Job Details Landing Page</h1>
+  <!-- Passes id from url as a param of $route -->
+  <p>The job id is {{ $route.params.id }}</p>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
