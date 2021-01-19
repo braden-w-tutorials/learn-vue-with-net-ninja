c91d3f3393c8224dbf1085dc3e591f067448505e
Braden <bmw02002turbo@gmail.com>
2021-01-18 23:52:35 -0800

    Added v-if and v-else for loading jobs

--- ninja-jobs/src/views/JobDetails/Jobs.vue
+++ ninja-jobs/src/views/JobDetails/Jobs.vue
@@ -1,12 +1,15 @@
 <template>
   <h1>Jobs</h1>
-  <div v-for="job in jobs" :key="job.id">
-    <router-link :to="{ name: 'JobDetails', params: { id: job.id } }">
-      <h2>
-        {{ job.title }}
-      </h2>
-    </router-link>
+  <div v-if="jobs.length">
+    <div v-for="job in jobs" :key="job.id">
+      <router-link :to="{ name: 'JobDetails', params: { id: job.id } }">
+        <h2>
+          {{ job.title }}
+        </h2>
+      </router-link>
+    </div>
   </div>
+  <div v-else>Loading jobs...</div>
 </template>
 
 <script>
