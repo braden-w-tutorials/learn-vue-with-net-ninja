0df1801ba8e0878880b808d2e790e63c5bc34cf8
Braden <bmw02002turbo@gmail.com>
2021-01-18 20:42:46 -0800

    Will only add if not already in array

--- web-forms/src/components/SignupForm.vue
+++ web-forms/src/components/SignupForm.vue
@@ -49,7 +49,9 @@ export default {
   methods: {
     addSkill(e) {
       if (e.key === "," && this.tempSkill) {
-        this.skills.push(this.tempSkill);
+        if (!this.skills.includes(this.tempSkill)) {
+          this.skills.push(this.tempSkill);
+        }
         this.tempSkill = "";
       }
     }
