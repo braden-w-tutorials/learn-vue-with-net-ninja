83f122096d0e7471a3fc5003e5a53143b8a8e82e
Braden <bmw02002turbo@gmail.com>
2021-01-18 22:09:54 -0800

    Added catchall 404

--- ninja-jobs/src/router/index.js
+++ ninja-jobs/src/router/index.js
@@ -3,6 +3,7 @@ import Home from "../views/Home.vue"
 import About from "../views/About.vue"
 import Jobs from "../views/JobDetails/Jobs.vue"
 import JobDetails from "../views/JobDetails/JobDetails.vue"
+import NotFound from "../views/NotFound.vue"
 const routes = [
   {
     path: "/",
@@ -30,6 +31,12 @@ const routes = [
   {
     path: "/all-jobs",
     redirect: "/jobs",
+  },
+  // catchall for 404
+  {
+    path: "/:catchAll(.*)",
+    name: "NotFound",
+    component: NotFound,
   },
 ]
 
