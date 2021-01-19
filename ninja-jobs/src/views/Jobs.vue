cd69fc27ae9d861606f35705c9c517924885b5dd
Braden <bmw02002turbo@gmail.com>
2021-01-18 21:46:42 -0800

    Added jobs.vue

---
+++ ninja-jobs/src/views/Jobs.vue
@@ -0,0 +1,20 @@
+<template>
+  <h1></h1>
+  <div v-for="job in jobs" :key="job.id">{{ job.title }}</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      jobs: [
+        { title: "Ninja UX Designer", id: 1, details: "lorem" },
+        { title: "Ninja Web Developer", id: 2, details: "lorem" },
+        { title: "Ninja Vue Develoepr", id: 3, details: "lorem" },
+      ],
+    }
+  },
+}
+</script>
+
+<style></style>
