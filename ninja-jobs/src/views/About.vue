562babd29d6ea7b49a24aeb5d4f47b91932c03b9
Braden <bmw02002turbo@gmail.com>
2021-01-18 21:42:14 -0800

    Added text to About.vue

--- ninja-jobs/src/views/About.vue
+++ ninja-jobs/src/views/About.vue
@@ -1,5 +1,32 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>Homepage</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem
+      alias veniam fugit! Quae, quam molestias? Corporis facere fugit quo
+      laborum sequi, libero quisquam voluptatibus temporibus molestias
+      necessitatibus itaque similique ratione quis sapiente soluta omnis minima
+      ex aliquid? Facilis cupiditate eaque beatae itaque iusto nam ex quisquam
+      natus. Laudantium quam quo, labore quae in aperiam voluptas temporibus. A
+      recusandae in sapiente dolores. Amet reprehenderit incidunt at non eaque.
+      Vel ad cumque voluptates enim sunt aperiam. Veritatis consequatur
+      reiciendis cum enim, cupiditate commodi perspiciatis ut, distinctio
+      necessitatibus tempora ea temporibus saepe. Totam voluptate quae illum
+      voluptatum architecto recusandae quam dolor nulla necessitatibus.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem
+      alias veniam fugit! Quae, quam molestias? Corporis facere fugit quo
+      laborum sequi, libero quisquam voluptatibus temporibus molestias
+      necessitatibus itaque similique ratione quis sapiente soluta omnis minima
+      ex aliquid? Facilis cupiditate eaque beatae itaque iusto nam ex quisquam
+      natus. Laudantium quam quo, labore quae in aperiam voluptas temporibus. A
+      recusandae in sapiente dolores. Amet reprehenderit incidunt at non eaque.
+      Vel ad cumque voluptates enim sunt aperiam. Veritatis consequatur
+      reiciendis cum enim, cupiditate commodi perspiciatis ut, distinctio
+      necessitatibus tempora ea temporibus saepe. Totam voluptate quae illum
+      voluptatum architecto recusandae quam dolor nulla necessitatibus.
+    </p>
+    >
   </div>
 </template>
