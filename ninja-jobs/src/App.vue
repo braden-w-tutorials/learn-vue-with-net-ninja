7f91b08949247a782b50131edfb19ea6657ea980
Braden <bmw02002turbo@gmail.com>
2021-01-18 21:39:46 -0800

    Used data binding for routing

--- ninja-jobs/src/App.vue
+++ ninja-jobs/src/App.vue
@@ -1,9 +1,10 @@
 <template>
   <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/">Home</router-link>
+    <!-- Use data binding to match with name -->
+    <router-link :to="{ name: 'About' }">About</router-link>
   </div>
-  <router-view/>
+  <router-view />
 </template>
 
 <style>
