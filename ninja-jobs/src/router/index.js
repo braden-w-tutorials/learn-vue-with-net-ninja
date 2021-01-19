6c454ec37f58976008b5a622ddb55cdec45452d0
Braden <bmw02002turbo@gmail.com>
2021-01-18 21:47:54 -0800

    Import Jobs

--- ninja-jobs/src/router/index.js
+++ ninja-jobs/src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router"
 import Home from "../views/Home.vue"
 import About from "../views/About.vue"
+import Jobs from "../views/Jobs.vue"
 const routes = [
   {
     path: "/",
