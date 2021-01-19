3b624443fd1fdb62f45f82df7c76cd25f17c5f27
Braden <bmw02002turbo@gmail.com>
2021-01-18 20:19:50 -0800

    Initialized Vue App

---
+++ web-forms/src/components/SignupForm.vue
@@ -0,0 +1,13 @@
+<template>
+  <form>
+    <label><Email:/label></label>
+    <input type="email" name="E-mail" required />
+  </form>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
