f10c7d59bb58631faeebc4b2040a1dd8ad63463f
Braden <bmw02002turbo@gmail.com>
2021-01-18 21:42:18 -0800

    Added to Home.vue

--- ninja-jobs/src/views/Home.vue
+++ ninja-jobs/src/views/Home.vue
@@ -1,18 +1,40 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Homepage</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem
+      alias veniam fugit! Quae, quam molestias? Corporis facere fugit quo
+      laborum sequi, libero quisquam voluptatibus temporibus molestias
+      necessitatibus itaque similique ratione quis sapiente soluta omnis minima
+      ex aliquid? Facilis cupiditate eaque beatae itaque iusto nam ex quisquam
+      natus. Laudantium quam quo, labore quae in aperiam voluptas temporibus. A
+      recusandae in sapiente dolores. Amet reprehenderit incidunt at non eaque.
+      Vel ad cumque voluptates enim sunt aperiam. Veritatis consequatur
+      reiciendis cum enim, cupiditate commodi perspiciatis ut, distinctio
+      necessitatibus tempora ea temporibus saepe. Totam voluptate quae illum
+      voluptatum architecto recusandae quam dolor nulla necessitatibus.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem
+      alias veniam fugit! Quae, quam molestias? Corporis facere fugit quo
+      laborum sequi, libero quisquam voluptatibus temporibus molestias
+      necessitatibus itaque similique ratione quis sapiente soluta omnis minima
+      ex aliquid? Facilis cupiditate eaque beatae itaque iusto nam ex quisquam
+      natus. Laudantium quam quo, labore quae in aperiam voluptas temporibus. A
+      recusandae in sapiente dolores. Amet reprehenderit incidunt at non eaque.
+      Vel ad cumque voluptates enim sunt aperiam. Veritatis consequatur
+      reiciendis cum enim, cupiditate commodi perspiciatis ut, distinctio
+      necessitatibus tempora ea temporibus saepe. Totam voluptate quae illum
+      voluptatum architecto recusandae quam dolor nulla necessitatibus.
+    </p>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+  name: "Home",
+  components: {},
 }
 </script>
