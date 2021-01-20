825fd542ef4a366dbc39cb666242d75866f40b9f
Braden <bmw02002turbo@gmail.com>
2021-01-19 19:14:36 -0800

    Formatting

--- dojo-blog/src/views/Home.vue
+++ dojo-blog/src/views/Home.vue
@@ -6,21 +6,22 @@
 </template>
 
 <script>
-import {ref} from 'vue'
+import { ref } from "vue"
 
 // @ is an alias to /src
 
 export default {
   name: "Home",
   setup() {
-  console.log('setup')
-  const p = ref(null)
-  // These are not reactive values
-  let name = 'mario'
-  let age = 30
-  const handleClick = () => {
-    console.log("p")
-  }
-  return{name, age, handleClick},
+    console.log("setup")
+    const p = ref(null)
+    // These are not reactive values
+    let name = "mario"
+    let age = 30
+    const handleClick = () => {
+      console.log(p, p.value)
+    }
+    return { name, age, handleClick }
+  },
 }
 </script>
