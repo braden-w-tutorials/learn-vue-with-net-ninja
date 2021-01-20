40edef1d3c448ee061d7b2ce7bfd7729abd32645
Braden <bmw02002turbo@gmail.com>
2021-01-20 09:48:38 -0800

    New refs for reactivity

--- dojo-blog/src/views/Home.vue
+++ dojo-blog/src/views/Home.vue
@@ -15,16 +15,25 @@ export default {
   name: "Home",
   setup() {
     console.log("setup")
-    const p = ref(null)
+    // const p = ref(null)
+
     // These are not reactive values
-    let name = "mario"
-    let age = 30
+    // let name = "mario"
+    // let age = 30
+
+    //These are reactive values
+    const name = ref("mario")
+    const age = ref(30)
+
     const handleClick = () => {
-      console.log(p, p.value)
-      p.value.classList.add("test")
-      p.value.textContent = "hello, ninjas"
+      // console.log(p, p.value)
+      // p.value.classList.add("test")
+      // p.value.textContent = "hello, ninjas"
+
+      name.value = "luigi"
+      age.value = 35
     }
-    return { name, age, handleClick, p }
+    return { name, age, handleClick }
   },
 }
 </script>
